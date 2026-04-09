2cb91b094e69af434f43816f7311e8c2b4cbd5ed
alonadoli <99336720+alonadoli@users.noreply.github.com>
2026-04-09 17:47:20 +0100

    Set version to 1.0.0 for initial PyPI release

--- groupappeals/__init__.py
+++ groupappeals/__init__.py
@@ -23,7 +23,7 @@ from .device_utilities import determine_compute_device, convert_device_to_pipeli
 # Full pipeline import
 from .fullpipeline import run_full_pipeline
 
-__version__ = "1.0.8"
+__version__ = "1.0.0"
 
 # CLI entry point
 def cli_main():
